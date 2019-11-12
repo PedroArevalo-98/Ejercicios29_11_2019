2843923397f01b9ab659175d3f33104da5e6b52c
pedro <pedro.arevalo.alcaide@gmail.com>
2019-11-12 23:03:07 +0100

    primer commit

---
+++ src/Ej_2_Vehiculo/MainVehiculo.java
@@ -0,0 +1,13 @@
+package Ej_2_Vehiculo;
+
+public class MainVehiculo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vehiculo v1 = new Vehiculo("Golf");
+		v1.setpotencia(2000);
+		v1.setcRuedas(false);
+		v1.Imprimirdatos();
+	}
+
+}
